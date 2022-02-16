486a7430cff2dc1e1ad3a76417dcbe5bf48ea8c8
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-02-16 10:43:45 -0300

    fix issue 2

--- background.js
+++ background.js
@@ -4,7 +4,26 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Arthur Paulino
 */
 
-const SET_ICON_PATH = "SET_ICON_PATH";
+const SET_ICON_PATH  = "SET_ICON_PATH";
+const INSTALL_REASON = "install";
+
+const ESCAPE_CHAR_KEY = "escape_char";
+const URL_MATCHES_KEY = "url_matches";
+const IS_ON_KEY       = "is_on";
+
+const DEFAULT_SETTINGS = {};
+DEFAULT_SETTINGS[ESCAPE_CHAR_KEY] = "\\";
+DEFAULT_SETTINGS[IS_ON_KEY] = true;
+DEFAULT_SETTINGS[URL_MATCHES_KEY] = [
+    "https://leanprover.zulipchat.com",
+    "https://github.com/leanprover-community/mathlib"
+];
+
+chrome.runtime.onInstalled.addListener(function(details){
+    if (details.reason === "install") {
+        chrome.storage.sync.set(DEFAULT_SETTINGS);
+    }
+});
 
 chrome.runtime.onMessage.addListener(
     (request, sender, _) => {
